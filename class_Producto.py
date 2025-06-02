3a01a10ee7ff7f4c54b93e197d9c6db6d640f2be
sofi3224 <sofia.reynaga.et7@gmail.com>
2025-06-02 17:21:36 -0300

    Agrego funcion verDetalleProducto y verProductoCategoria

--- class_Producto.py
+++ class_Producto.py
@@ -1,14 +1,23 @@
 class Producto:
-    def __init__(self, id_producto, nombre, descripcion, precio, stock, categoria ):
+    def __init__(self, id_producto, nombre, descripcion, precio, stock, categoria, talles, colores ):
         self.id_producto = id_producto
         self.nombre = nombre
         self.descripcion = descripcion
         self.precio = precio
         self.stock = stock
         self.categoria = categoria
+        self.talles = talles if talles else[]
+        self.colores = colores if colores else[]
 
-    def actualizarStock(self, cantidad):
-        self.stock -= cantidad
+    def __str__(self):
+        return f"{self.nombre} - ${self.precio}"
+
     def mostrarDetalles(self):
-        return f"{self.nombre} - {self.descripcion} - {self.precio}"
-    
+        return(
+            f"Nombre: {self.nombre}\n"
+            f"Descripcion: {self.descripcion}\n"
+            f"Precio: {self.precio}\n"
+            f"Talles Disponibles: {', '.join(self.talles)}\n"
+            f"Colores Disponibles: {', '.join(self.colores)}\n"
+            f"Stock Disponible: {self.stock}\n"
+        )
